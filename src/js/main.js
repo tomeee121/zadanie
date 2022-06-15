9946cd6457008849582904807e87f472dc293d64
Tomasz Borowski <bortom112233@gmail.com>
2022-06-15 21:33:29 +0200

    calculating clicked event

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,11 @@ let closeButton = document.getElementById("close-button");
 let overlay = document.getElementById("overlay");
 let popUp = document.getElementById("popUp");
 
+let backgroundOfPopUp = document.querySelector(":not(#popUp)");
+let header = document.getElementById('mainHeader');
+let ifPoppedUp = false;
+
+let clickCounter = 0;
 
 openButton.addEventListener('click',() => {
 popUp.style.transform = "translate(-50%, -50%) scale(1)";
@@ -13,7 +18,28 @@ overlay.style.right = "0";
 overlay.style.bottom = "0";
 overlay.style.opacity = "1";
 overlay.style.backgroundColor = "rgba(0, 0, 0, .5)";
-overlay.style.pointerEvents = "none";});
+overlay.style.pointerEvents = "all";
+ifPoppedUp = true;});
 
+if(ifPoppedUp){
+    popUp.closest.addEventListener(onclick,() => {
+    alert('sdf')})
+    }
+
+closeButton.addEventListener('click',() => {
+    popUp.style.transform = "translate(-50%, -50%) scale(0)";
+    overlay.style.opacity = "0";});
+
+function clickCounter()  
+{
+    clickCounter++;
+    localStorage.setItem("clickCounter", clickCounter);
+
+    let popUpBody = document.querySelector(".pop-up-body");
+    let p = document.createElement("span");
+    let text = document.createTextNode(localStorage.getItem("clickCounter"));
+    p.append(text);
+    popUpBody.append(p);
+}    
                                   
 
